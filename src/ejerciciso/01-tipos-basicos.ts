6827d633a243430c268888a720e8ec26bb1a3b94
Fernando Torres <fernandot371@gmail.com>
2021-05-26 23:09:48 -0500

    sec3 v13 respaldo

---
+++ src/ejerciciso/01-tipos-basicos.ts
@@ -0,0 +1,14 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre1 : string = "strider";
+let hp1 : number | string = 95;
+let estaVivo1: boolean = true;
+
+nombre1 = "fernando";
+
+hp1 = 'FULL';
+
+console.log(nombre1, hp1, estaVivo1);
+
