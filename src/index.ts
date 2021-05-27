d1c642ac285ccd5dc6ceb3d9a10fefd4cb2f935e
Fernando Torres <fernandot371@gmail.com>
2021-05-26 23:05:11 -0500

    sec1-3 v12

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre : string = "strider";
+let hp : number | string = 95;
+
+nombre = "fernando";
+
+hp = 'FULL';
+
+console.log(nombre, hp);
+
