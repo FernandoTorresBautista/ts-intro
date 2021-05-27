e1cf6ff4dea78df698df67dc21cd95fb006c84de
Fernando Torres <fernandot371@gmail.com>
2021-05-26 23:08:03 -0500

    sec3 v13

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@
 
 let nombre : string = "strider";
 let hp : number | string = 95;
+let estaVivo: boolean = true;
 
 nombre = "fernando";
 
 hp = 'FULL';
 
-console.log(nombre, hp);
+console.log(nombre, hp, estaVivo);
 
